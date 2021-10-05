399d4154f1f73001042e0242bb7b9e48543f3cd2
Alex Miruk <kws.nightmen@gmail.com>
2021-10-06 00:43:04 +0300

    Lesson_2_complete

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-
-const $goodsList = document.querySelector('.goods-list');
-  
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item card" style="width: 18rem;"><h3  class="card-title">${title}</h3><p class="card-text">${price}</p></div>`;
-};
-  
-const renderGoodsList = (list = goods) => {
-    let goodsList = list.map(
-            item => renderGoodsItem(item)
-        ).join('');
-
-    $goodsList.insertAdjacentHTML('beforeend', goodsList);
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
 }
 
-// renderGoodsList();
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+
+    costAllGoods(){
+        let sum = 0;
+        this.goods.forEach (good => {
+            sum += good.price
+        });
+        return sum
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        listHtml += `<div class="goods-item_coast"><p>Итоговая цена товаров = ${this.costAllGoods()}</p</div>`
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+}
+
+class Basket extends GoodsList {
+
+    addBasketItem() {null}
+    deleteBasketItem(){null}
+
+}
+
+class  GoodsBasket extends GoodsItem {
+    constructor() {
+        super();
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
